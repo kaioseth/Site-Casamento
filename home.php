fec2af2c69bd3f344b2b90e54aaf51cb0f1b99f2
Kaio Nascimento <kaio.seth@gmail.com>
2018-06-10 23:57:34 -0300

    iniciando montagem do site

---
+++ home.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+        <div class="display-t">
+            <div class="display-tc animate-box" data-animate-effect="fadeIn">
+                <h1>Aline &amp; Kalil</h1>
+                <h2>Vamos nos casar *-* (Finalmente)</h2>
+                <div class="simply-countdown simply-countdown-one"></div>
+                <p><a href="#" class="btn btn-default btn-sm">Reserve essa data</a></p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ noivos.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+            <h2>Olá!</h2>
+            <h3>em 12 de Outubro de 2018, Bonito - MS</h3>
+            <p>Nós o convidamos para celebrar conosco</p>
+        </div>
+    </div>
+    <div class="couple-wrap animate-box">
+        <div class="couple-half">
+            <div class="groom">
+                <img src="images/groom.jpg" alt="groom" class="img-responsive">
+            </div>
+            <div class="desc-groom">
+                <h3>Aline Santos</h3>
+                <p>texto de apresentação</p>
+            </div>
+        </div>
+        <p class="heart text-center"><i class="icon-heart2"></i></p>
+        <div class="couple-half">
+            <div class="bride">
+                <img src="images/bride.jpg" alt="groom" class="img-responsive">
+            </div>
+            <div class="desc-bride">
+                <h3>Kalil Mirandola Mustafa</h3>
+                <p>texto de apresentação</p>
+            </div>
+        </div>
+    </div>
+</div>
